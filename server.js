17fc536f09d8a9ec5d3ad239d6903ec269573e85
EmberVoid <jeancarlo199797@gmail.com>
2018-05-16 15:59:25 -0600

    /profile:id /image

--- server.js
+++ server.js
@@ -53,13 +53,38 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1])
 });
 
-/*
-*  / --> res = Working
-*  /signin --> POST = success/fail
-*   /register --> POST = user
-*   /profile/:userID --> GET = user
-*   /image --> PUT --> user
-* */
+app.get ('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id){
+            found = true;
+            return res.json(user);
+        }
+    });
+
+    if (!found){
+        res.status(404).json('No user');
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id){
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+
+    if (!found){
+        res.status(404).json('No user');
+    }
+});
 
 app.listen(port, () => {
    console.log("Server running on port ", port)
